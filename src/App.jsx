b6ed53f2823ef58387c0798c3471ac50db90d461
Ian Cenry <iancenry@gmail.com>
2023-03-18 00:28:26 +0300

    update openai image generator

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import { Configuration, OpenAIApi} from 'openai'
 
 function App() {
+  const [prompt, setPrompt] = useState("")
+  const [result, setResult] = useState("")
+
   const configuration = new Configuration({
     apiKey: import.meta.env.VITE_OPENAI_API_KEY,
   });  
@@ -10,18 +13,23 @@ function App() {
 
   const generateImage = async ()=>{
     const res = await openai.createImage({
-      prompt: "Say this is a test",
+      prompt: prompt,
       n: 1,
       size: "1024x1024"
     });
 
-    console.log(res.data.data[0].url)
+    setResult(res.data.data[0].url)
 
   }
 
+
   return (
-    <div>
+    <div className='main'>
+      <h1>Generate an AI  Image</h1>
+      <input type="text" placeholder='Type Image Query' className='main-input' name='prompt' value={prompt} onChange={(e) => setPrompt(e.target.value)} />
       <button onClick={generateImage}>Generate an Image</button>
+
+      {result.length > 0 && <img src={result} alt="result" className='result-image' />}
     </div>
   )
 }
